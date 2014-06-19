6f37fe24200b16d263c665e68e3212b39aa6a1a9
DannyXu <dannyxu2014@163.com>
2014-06-19 15:26:25 +0800

    g1

--- gitcommit.bat
+++ gitcommit.bat
@@ -1,3 +1,14 @@
+@echo off
+if {%1}=={} goto noparams
 git add .
 git commit -a -m %1
 git push origin master
+goto ok
+:noparams
+echo Usage: gitcommit your-commit-comment
+goto end
+:ok
+echo Done!
+:end
+
+
