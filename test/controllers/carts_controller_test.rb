c34a8a1dec68b71090df53d9c39ba0f4355e634e
DannyXu <dannyxu2014@163.com>
2014-06-19 15:17:02 +0800

    g1

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -11,11 +11,6 @@ class CartsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:carts)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
   test "should create cart" do
     assert_difference('Cart.count') do
       post :create, cart: {  }
@@ -47,4 +42,19 @@ class CartsControllerTest < ActionController::TestCase
 
     assert_redirected_to store_path
   end
+
+  test "requires item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty'
+  end
+
+  test "should get new" do
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    LineItem.create(:cart => cart, :product => products(:ruby))
+    get :new
+    assert_response :success
+  end
+
 end
