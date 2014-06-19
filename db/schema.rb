f91c6674fbb287445f65f49eeff96ace5e26fe7b
DannyXu <dannyxu2014@163.com>
2014-06-19 17:44:01 +0800

    g3play

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619091256) do
+ActiveRecord::Schema.define(version: 20140619094329) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20140619091256) do
     t.string   "name"
     t.text     "address"
     t.string   "email"
-    t.string   "pay_type"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "pay_type_id"
