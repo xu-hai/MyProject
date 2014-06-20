16dcc27e66d4dd7a43c77f80b032576860a18ed6
xu-hai <dannyxu2014@163.com>
2014-06-20 11:51:03 +0800

    H1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -73,10 +73,9 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
 
         session[:cart_id] = nil
+        Notifier.order_received(@order).deliver
 
-        format.html { redirect_to store_url, notice:
-
-                      'Thank you for your order.' }
+        format.html { redirect_to store_url, notice:'Thank you for your order.' }
 
         format.json { render :show, status: :created, location: @order }
 
